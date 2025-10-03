8ae432ce63998043788a5838ddab535f0855691c
TrishBatra18 <trish.idesign.market@gmail.com>
2025-10-03 20:23:34 +0530

    ci-cd-test

--- src/app/cicd/page.tsx
+++ src/app/cicd/page.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page to test ci-cd working or not</div>
+    <div>page to test ci-cd working or not
+        <ul>test-2</ul>
+        <ul>test-2</ul>
+        <ul>test-2</ul>
+        <ul>test-2</ul>
+    </div>
   )
 }
 
